82297ef7bbd950844d828b672c14d4f78a6fdb40
minhduc5a15 <minhduc5a15@gmail.com>
2025-04-17 00:53:38 +0700

    up

--- insert_khach_hang.sql
+++ insert_khach_hang.sql
@@ -1,10 +1,10 @@
 use pawnshop;
 
 INSERT INTO KHACH_HANG (MA_KHACH_HANG, CCCD, HO_TEN, DIEN_THOAI, DIA_CHI, EMAIL)
-VALUES ('KH001', '012345678901', 'Nguyễn Văn A', '0912345678', '123 Đường Lê Lợi, Q.1, TP.HCM', 'nguyenvana@gmail.com'),
-       ('KH002', '098765432109', 'Trần Thị B', '0987654321', '456 Đường Nguyễn Trãi, Q.5, TP.HCM', 'tranthib@gmail.com'),
-       ('KH003', '034567890123', 'Lê Hoàng C', '0934567890', '789 Đường Phan Xích Long, Q.Phú Nhuận, TP.HCM', 'lehoangc@gmail.com'),
-       ('KH004', '056789012345', 'Phạm Mỹ D', '0909876543', '99 Đường Hai Bà Trưng, Q.3, TP.HCM', 'phammyd@gmail.com'),
-       ('KH005', '123456789012', 'Võ Minh E', '0989123456', '12 Đường Nguyễn Văn Cừ, Q.10, TP.HCM', 'vominhe@gmail.com'),
-       ('KH006', '234567890123', 'Đặng Thị F', '0913567891', '486 Đường Võ Văn Ngân, TP.Thủ Đức, TP.HCM', 'dangthif@gmail.com'),
-       ('KH007', '345678901234', 'Bùi Anh G', '0901234567', '17 Đường Âu Cơ, Q.Tân Bình, TP.HCM', 'buianhg@gmail.com');
+VALUES ('KH001', '012345678901', N'Nguyễn Văn A', '0912345678', N'123 Đường Lê Lợi, Q.1, TP.HCM', 'nguyenvana@gmail.com'),
+       ('KH002', '098765432109', N'Trần Thị B', '0987654321', N'456 Đường Nguyễn Trãi, Q.5, TP.HCM', 'tranthib@gmail.com'),
+       ('KH003', '034567890123', N'Lê Hoàng C', '0934567890', N'789 Đường Phan Xích Long, Q.Phú Nhuận, TP.HCM', 'lehoangc@gmail.com'),
+       ('KH004', '056789012345', N'Phạm Mỹ D', '0909876543', N'99 Đường Hai Bà Trưng, Q.3, TP.HCM', 'phammyd@gmail.com'),
+       ('KH005', '123456789012', N'Võ Minh E', '0989123456', N'12 Đường Nguyễn Văn Cừ, Q.10, TP.HCM', 'vominhe@gmail.com'),
+       ('KH006', '234567890123', N'Đặng Thị F', '0913567891', N'486 Đường Võ Văn Ngân, TP.Thủ Đức, TP.HCM', 'dangthif@gmail.com'),
+       ('KH007', '345678901234', N'Bùi Anh G', '0901234567', N'17 Đường Âu Cơ, Q.Tân Bình, TP.HCM', 'buianhg@gmail.com');
--- insert_nhan_vien.sql
+++ insert_nhan_vien.sql
@@ -2,10 +2,10 @@ use pawnshop;
 
 INSERT INTO NHAN_VIEN (MA_NHAN_VIEN, CCCD, HO_TEN, DIEN_THOAI, DIA_CHI, EMAIL, CHUC_VU, LUONG)
 VALUES
-('NV001', '012345678901', 'Nguyễn Văn A', '0912345678', '123 Đường Lê Lợi, Q.1, TP.HCM', 'nvana@gmail.com', 'Giám Đốc', 30000000),
-('NV002', '098765432109', 'Trần Thị B', '0987654321', '456 Đường Nguyễn Trãi, Q.5, TP.HCM', 'nvb@gmail.com', 'Quản Lý', 20000000),
-('NV003', '034567890123', 'Lê Hoàng C', '0934567890', '789 Đường Phan Xích Long, Q.Phú Nhuận, TP.HCM', 'nvc@gmail.com', 'Nhân Viên Bán Hàng', 15000000),
-('NV004', '056789012345', 'Phạm Mỹ D', '0909876543', '99 Đường Hai Bà Trưng, Q.3, TP.HCM', 'nvd@gmail.com', 'Nhân Viên Bán Hàng', 15000000),
-('NV005', '123456789012', 'Võ Minh E', '0989123456', '12 Đường Nguyễn Văn Cừ, Q.10, TP.HCM', 'nve@gmail.com', 'Kế Toán', 18000000),
-('NV006', '234567890123', 'Đặng Thị F', '0913567891', '486 Đường Võ Văn Ngân, TP.Thủ Đức, TP.HCM', 'nvf@gmail.com', 'Hành Chính', 16000000),
-('NV007', '345678901234', 'Bùi Anh G', '0901234567', '17 Đường Âu Cơ, Q.Tân Bình, TP.HCM', 'nvg@gmail.com', 'Bảo Vệ', 12000000);
+('NV001', '012345678901', N'Nguyễn Văn A', '0912345678', N'123 Đường Lê Lợi, Q.1, TP.HCM', 'nvana@gmail.com', N'Giám Đốc', 30000000),
+('NV002', '098765432109', N'Trần Thị B', '0987654321', N'456 Đường Nguyễn Trãi, Q.5, TP.HCM', 'nvb@gmail.com', N'Quản Lý', 20000000),
+('NV003', '034567890123', N'Lê Hoàng C', '0934567890', N'789 Đường Phan Xích Long, Q.Phú Nhuận, TP.HCM', 'nvc@gmail.com', N'Nhân Viên Bán Hàng', 15000000),
+('NV004', '056789012345', N'Phạm Mỹ D', '0909876543', N'99 Đường Hai Bà Trưng, Q.3, TP.HCM', 'nvd@gmail.com', N'Nhân Viên Bán Hàng', 15000000),
+('NV005', '123456789012', N'Võ Minh E', '0989123456', N'12 Đường Nguyễn Văn Cừ, Q.10, TP.HCM', 'nve@gmail.com', N'Kế Toán', 18000000),
+('NV006', '234567890123', N'Đặng Thị F', '0913567891', N'486 Đường Võ Văn Ngân, TP.Thủ Đức, TP.HCM', 'nvf@gmail.com', N'Hành Chính', 16000000),
+('NV007', '345678901234', N'Bùi Anh G', '0901234567', N'17 Đường Âu Cơ, Q.Tân Bình, TP.HCM', 'nvg@gmail.com', N'Bảo Vệ', 12000000);
